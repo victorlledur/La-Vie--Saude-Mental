83770dd7b74b18fabe08c55d188a4da922da3749
Victor Ledur <victorlledur@gmail.com>
2022-10-14 19:20:43 -0300

    Estrutura inicial do projeto

---
+++ src/database/index.js
@@ -0,0 +1,33 @@
+const Sequelize = require("sequelize");
+
+const DB_NAME = "";
+const DB_USER = "root";
+const DB_PASS = "";
+const DB_CONFIG = {
+    dialect: "mysql",
+    host: "localhost",
+    port: 3306
+}
+//objeto para guardar a conexão do banco de dados
+let db = {};
+
+try{
+    db = new Sequelize(DB_NAME, DB_USER, DB_PASS, DB_CONFIG);
+}catch(error){
+    console.error("Erro ao tentar uma conexao com o banco de dados");
+}
+
+async function hasConection(){
+    try {
+        await db.authenticate();
+        console.log("Banco de dados conectado")
+    } catch (error) {
+        console.error("Erro ao tentar se conectar")
+    }
+}
+
+Object.assign(db, {
+    hasConection,
+})
+
+module.exports = db;
