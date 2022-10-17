9e42e15aa7f32172b24a0d98989b08593a03e73c
MailtonOliviera <mailton.s.oliveira@gmail.com>
2022-10-17 00:24:26 -0300

    feat: adicionando validação em psicologos

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,18 @@ const express = require("express");
 const psicologosController = require("../controllers/psicologosController");
 const atendimentosController = require("../controllers/atendimentosController");
 const loginValidation = require("../validations/login/login");
+const psicologosCreateValidation = require("../validations/psicologos/create");
+const onepsicologoValidation = require("../validations/psicologos/getOne");
+const updatepsicologoValidation = require("../validations/psicologos/update");
 const authController = require("../controllers/authController");
 const routes = express.Router();
 
 routes.post("/login", loginValidation, authController.login);
 
 routes.get("/psicologos", psicologosController.listPsicologos);
-routes.get("/psicologo/:id", psicologosController.onePsicologo);
-routes.post("/psicologo/criar", psicologosController.createPsicologo);
-routes.put("/psicologo/:id", psicologosController.updatePsicologo);
+routes.get("/psicologo/:id", onepsicologoValidation, psicologosController.onePsicologo);
+routes.post("/psicologo/criar", psicologosCreateValidation, psicologosController.createPsicologo);
+routes.put("/psicologo/:id", updatepsicologoValidation, psicologosController.updatePsicologo);
 routes.delete("/psicologo/:id", psicologosController.deletePsicologo);
 
 routes.get("/atendimentos", atendimentosController.listarAtendimentos);
