b623e9ef954f11a420be2e454024fab3ede8e79a
Victor Ledur <victorlledur@gmail.com>
2022-10-20 02:06:08 -0300

    update: routes validations

--- src/routes/index.js
+++ src/routes/index.js
@@ -20,17 +20,17 @@ routes.get("/psicologos", psicologosController.listPsicologos);
 routes.get("/psicologo/:id", byIdValidation, psicologosController.byIdPsicologo);
 routes.post("/psicologo/criar", psicologoCreateValidation, psicologosController.createPsicologo);
 routes.put("/psicologo/:id", psicologoUpdateValidation, psicologosController.updatePsicologo);
-routes.delete("/psicologo/:id", psicologosController.deletePsicologo);
+routes.delete("/psicologo/:id", byIdValidation, psicologosController.deletePsicologo);
 
 routes.get("/atendimentos", atendimentosController.listAtendimentos);
-routes.get("/atendimento/:id", atendimentosController.byIdAtendimentos);
+routes.get("/atendimento/:id", byIdValidation, atendimentosController.byIdAtendimentos);
 routes.post("/atendimento/criar", auth, atendimentoCreateValidation, atendimentosController.createAtendimento);
 
 routes.get("/pacientes", pacientesController.listPacientes);
 routes.get("/paciente/:id", byIdValidation, pacientesController.byIdPaciente);
 routes.post("/paciente/criar", pacienteCreateValidation, pacientesController.createPaciente);
 routes.put("/paciente/:id", pacienteUpdateValidation, pacientesController.updatePaciente);
-routes.delete("/paciente/:id", pacientesController.deletePaciente);
+routes.delete("/paciente/:id", byIdValidation, pacientesController.deletePaciente);
 
 routes.get("/dashboard/numero-pacientes", dashboardController.numberPacientes);
 routes.get("/dashboard/numero-atendimentos", dashboardController.numberAtendimentos);
